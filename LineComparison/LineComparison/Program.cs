ea3ae9199159483aa869525bc3d92f0457dadad0
qismat <qismatnisha6243@gmail.com>
2022-12-17 00:23:51 +0530

    [qismat] Add : UC1-CartesianSystem

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comprasion comprasion = new Comprasion(1, 2, 3, 4);
+            comprasion.CalculateLength();
+        }
+    }
+}
