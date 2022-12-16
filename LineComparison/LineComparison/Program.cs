d89e9bdce160cb4acc29565fb1f5af7637b5698a
qismat <qismatnisha6243@gmail.com>
2022-12-17 00:44:51 +0530

    [qismat] Add : UC2-CheckEqualityOfTwoLine

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -5,7 +5,17 @@
         static void Main(string[] args)
         {
             Comprasion comprasion = new Comprasion(1, 2, 3, 4);
-            comprasion.CalculateLength();
+            double line1 = comprasion.CalculateLength();
+            Console.WriteLine("Length of first line " + line1);
+            Comprasion LineComparisionProblem = new Comprasion(1, 2, 3, 4);
+            double line2 = LineComparisionProblem.CalculateLength();
+            Console.WriteLine("Length of second line" + line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("Line 1 and Line 2 are equal");
+            }
+            else
+                Console.WriteLine("Not equal");
         }
     }
 }
